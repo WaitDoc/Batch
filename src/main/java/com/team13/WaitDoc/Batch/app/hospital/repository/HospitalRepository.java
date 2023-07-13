a0e72fc389c0313273bef8be039a052a66630e41
Y39 <rla4878@gmail.com>
2023-07-13 17:49:08 +0900

    Feat: 청크를 이용한 data 저장 기능 구현

--- src/main/java/com/team13/WaitDoc/Batch/app/hospital/repository/HospitalRepository.java
+++ src/main/java/com/team13/WaitDoc/Batch/app/hospital/repository/HospitalRepository.java
@@ -2,11 +2,12 @@ package com.team13.WaitDoc.Batch.app.hospital.repository;
 
 import com.team13.WaitDoc.Batch.app.hospital.entity.Hospital;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.Optional;
 
 public interface HospitalRepository extends JpaRepository<Hospital, Long> {
-    public Optional<Hospital> findByHpid(String hpid);
+    Optional<Hospital> findByHpid(String hpid);
 
 
 
